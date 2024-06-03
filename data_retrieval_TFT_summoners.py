cdde8326aa3eeea648f05ddaa7f3a152ceb98075
petergorrr <75304586+petergorrr@users.noreply.github.com>
2024-06-03 13:47:50 +0800

    Update data_retrieval_TFT_summoners.py

--- data_retrieval_TFT_summoners.py
+++ data_retrieval_TFT_summoners.py
@@ -1,7 +1,3 @@
-"""
-This script is used to retrieve summoners data.
-"""
-
 import requests
 import pathlib
 import os
@@ -12,79 +8,65 @@ import sys
 region = sys.argv[1]
 APIKey = sys.argv[2]
 
-# URL region might have a different name
-if region == "kr":
-    url_region = "kr"
-elif region == "jp":
-    url_region = "jp1"
-elif region == "br":
-    url_region = "br1"
-elif region == "eune":
-    url_region = "eun1"
-elif region == "euw":
-    url_region = "euw1"
-elif region == "tr":
-    url_region = "tr1"
-elif region == "lan":
-    url_region = "la1"
-elif region == "las":
-    url_region = "la2"
-elif region == "oce":
-    url_region = "oc1"
-elif region == "ru":
-    url_region = "ru"
-elif region == "na":
-    url_region = "na"
+# Mapping of region name to API URL region
+region_mapping = {
+    "kr": "kr",
+    "jp": "jp1",
+    "br": "br1",
+    "eune": "eun1",
+    "euw": "euw1",
+    "tr": "tr1",
+    "lan": "la1",
+    "las": "la2",
+    "oce": "oc1",
+    "ru": "ru",
+    "na": "na"
+}
 
-# create a folder to store summoners' files later
-if os.path.isdir(f"TFT_summoners_data_{region}") is False:
-    os.mkdir(f"TFT_summoners_data_{region}")
+# Determine URL region
+url_region = region_mapping.get(region, region)
 
+# Create directory for summoners data if it does not exist
+summoners_data_dir = f"TFT_summoners_data_{region}"
+if not os.path.isdir(summoners_data_dir):
+    os.mkdir(summoners_data_dir)
 else:
     print(f"The TFT summoner folder of region {region} already exists.")
 
-
-# store summoners in a set
+# Store summoners in a set
 summoners = set()
 
-# Open JSON file by its directory path
+# Load summoners from JSON files
 for path in pathlib.Path(f"summoners_{region}").iterdir():
-    current_file = open(path, "r", encoding="utf-8")
-    data = json.loads(current_file.read())
-
-    # add the names into the set
-    for item in data:
-        summoners.add(item['name'])
-
+    with open(path, "r", encoding="utf-8") as current_file:
+        data = json.load(current_file)
+        for item in data:
+            summoners.add(item['name'])
     print("File loaded:", path)
 
 print("\nThe total number of summoners in this region:", len(summoners))
 
-
+# Process each summoner
 counter = 1
 for name in summoners:
     start_time = time.time()
 
-    # create URL
+    # Create URL
     URL = f"https://{url_region}.api.riotgames.com/tft/summoner/v1/summoners/by-name/{name}?api_key={APIKey}"
 
     try:
-        # request URL
+        # Request URL
         response = requests.get(URL)
+        responseJSON = response.json()
+
+        # Write the data into a JSON file and store it under the folder created earlier
+        with open(f"{summoners_data_dir}/summoner_{name}.json", "w", encoding='utf-8') as f:
+            json.dump(responseJSON, f, indent=4)
+
+        print(f"i:{counter}")
+        counter += 1
+        print("--- %s seconds ---" % (time.time() - start_time))
+        time.sleep(0.8)
 
     except Exception as e:
         print(e)
-
-    # get JSON of the data in the URL
-    responseJSON = response.json()
-
-    with open(f"TFT_summoners_data_{region}/summoner_{name}.json", "w", encoding='utf-8')as f:
-        json.dump(responseJSON, f, indent=4)
-
-    print(f"i:{counter}")
-
-    counter += 1
-
-    print("--- %s seconds ---" % (time.time() - start_time))
-
-    time.sleep(0.8)
